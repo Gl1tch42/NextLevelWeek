7d1365d1380b0fe9cd25a9483c3b4c709bde91b1
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2020-06-03 23:44:35 -0300

    att do dia 2

--- scripts/creatpoint.js
+++ scripts/creatpoint.js
@@ -20,6 +20,8 @@ function getCities(event) {
     const stateInput = document.querySelector("input[name=state")
     const ufValue = event.target.value
 
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
