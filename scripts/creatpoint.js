871dd42d57fdd22e3d6b6463aeb9a99e0f7bf9ce
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2020-06-03 23:43:38 -0300

    att do dia 2

--- scripts/creatpoint.js
+++ scripts/creatpoint.js
@@ -1,21 +1,42 @@
-function populatesUFs(){
-    const ufSelect = document.querySelector("select[name=uf]")
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
         .then(res => res.json())
         .then(states => {
 
-            for(const state of states){
+            for (state of states) {
                 ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
             }
-            
-    })
+        })
 }
 
-populatesUFs()
+
+populateUFs()
+
+
+function getCities(event) {
+    const citiesSelect = document.querySelector("select[name=city")
+    const stateInput = document.querySelector("input[name=state")
+    const ufValue = event.target.value
+
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(cities => {
+
+            for (city of cities) {
+                citiesSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+            citiesSelect.disabled = false
+        })
+
+}
+
+
 
 document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", ()=>{
-        Console.log("mudei")
-    })
+    .querySelector("select[name=uf")
+    .addEventListener("change", getCities)
